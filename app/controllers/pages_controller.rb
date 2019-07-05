5b15c8c358f72deb2f361cde5ca2a198f688e220
German Fagalde <germanfagalde@gmail.com>
2019-07-04 23:38:48 -0300

    add main pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def home; end
+
+  def news; end
+
+  def categories; end
+
+  def galleries; end
+
+  def about; end
+
+  def contact; end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
+
+  get '/noticias', to: 'pages#news', as: 'notice'
+  get '/categorias', to: 'pages#categories', as: 'categories'
+  get '/galerias', to: 'pages#galleries', as: 'galleries'
+  get '/nosotros', to: 'pages#about', as: 'about'
+  get '/contacto', to: 'pages#contact', as: 'contact'
+
 end
