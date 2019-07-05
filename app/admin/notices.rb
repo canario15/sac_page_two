ff08437e1b872aca9edf7d63e691e0c816be2429
German Fagalde <germanfagalde@gmail.com>
2019-07-05 03:05:04 -0300

    add image to Notice

--- app/admin/notices.rb
+++ app/admin/notices.rb
@@ -5,12 +5,13 @@ ActiveAdmin.register Notice do
 
   menu label: 'Noticias'
 
-  permit_params :title, :context
+  permit_params :title, :context, :image
 
   form do |f|
     f.inputs 'Noticia' do
       f.input :title
       f.input :context
+      f.input :image, as: :file
     end
     f.actions
   end
@@ -31,6 +32,9 @@ ActiveAdmin.register Notice do
       row :title
       row :created_at
       row :context
+      row :image do |ad|
+        image_tag ad.image.variant(resize: '200x200>') if ad.image.attached?
+      end
     end
   end
 end
--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,7 +1,21 @@
 class Notice < ApplicationRecord
+
+  has_one_attached :image
+
   validates :title, presence: true
   validates :title, length: { minimum: 6 }
   validates :context, presence: true
   validates :context, length: { minimum: 50 }
-  
+  validate :image_format
+
+  private
+
+  def image_format
+    return unless image.attached?
+    return if image.blob.content_type.start_with? 'image/'
+
+    image.purge_later
+    errors.add(:image, I18n.t('activerecord.errors.models.notice.attributes.image.format'))
+  end
+
 end
