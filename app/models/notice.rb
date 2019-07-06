3eb1ba1e8b10b115ca7727e2ac28036e04c9cfe2
German Fagalde <germanfagalde@gmail.com>
2019-07-06 06:47:21 -0300

    delete image format_validation code from notice

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -8,14 +8,4 @@ class Notice < ApplicationRecord
   validates :context, length: { minimum: 50 }
   validate :image_format
 
-  private
-
-  def image_format
-    return unless image.attached?
-    return if image.blob.content_type.start_with? 'image/'
-
-    image.purge_later
-    errors.add(:image, I18n.t('activerecord.errors.models.notice.attributes.image.format'))
-  end
-
 end
